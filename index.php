33df7d8295f636fde52afaac883bf7407d53702a
Ian Dobson <ian.dobson@economycarhire.com>
2015-01-08 16:22:39 +0000

    Title update

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <title>Nosbod.Net</title>
-        <meta name="description" content="Ian Dobson | Web Developer">
+        <meta name="description" content="Nosbod.net&trade;">
         <link rel="shortcut icon" href="/images/Anlogo.gif">
         <link rel="stylesheet" href="/css/base.css">
     </head>
