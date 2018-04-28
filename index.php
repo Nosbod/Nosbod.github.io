c6363a6ac042f9417aae89e1a8c4d03de28afc40
Ian <ian.dobson@gmail.com>
2018-04-28 18:29:17 +0100

    code tidy

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <title>nosbod.net</title>
     <meta name="description" content="nosbod.net">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="/images/animated-logo.gif">
     <link href="https://fonts.googleapis.com/css?family=Audiowide" rel="stylesheet">
     <link rel="stylesheet" href="/css/base.min.css">
